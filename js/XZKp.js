5f2a1a51207b783e1976251ae525ff83e9396d1a
Dragon Add <87432295+dragonadd@users.noreply.github.com>
2024-11-29 12:33:12 +0800

    Update XZKp.js

--- js/XZKp.js
+++ js/XZKp.js
@@ -2,13 +2,9 @@
 
 脚本功能：小作卡片——解锁VIP
 下载地址：https://is.gd/kO8498
-软件版本：2.7.3
-脚本作者：彭于晏💞
-更新时间：2024-3-10
-问题反馈：QQ+89996462
-QQ会员群：779392027💞
-TG反馈群：https://t.me/plus8889
-TG频道群：https://t.me/py996
+软件版本：3.5.1
+脚本原作者：彭于晏💞
+更新时间：2024-11-29
 使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 
 *******************************
@@ -16,10 +12,9 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/XZKp.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/dragonadd/Script/refs/heads/main/js/XZKp.js
 
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/XZKp.js
-
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/dragonadd/Script/refs/heads/main/js/XZKp.js
 [mitm] 
 
 hostname = api.revenuecat.com
